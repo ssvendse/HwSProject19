f5f885614162cd39e943cc33ff57b0aae9452df2
Skyler Svendsen <skylersvendsen@gmail.com>
2017-09-22 14:59:17 -0500

    first commit

---
+++ HwSProject19/Capital.swift
@@ -0,0 +1,13 @@
+//
+//  Capital.swift
+//  HwSProject19
+//
+//  Created by Skyler Svendsen on 9/22/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class Capital: NSObject {
+
+}
